c04152c1355e6bb43a4320e802d5714110ec7352
daurenabasov <daurenabasov987@gmail.com>
2023-03-28 12:48:25 +0600

    add layout, validation for registration

--- src/Interfaces/interfaces.ts
+++ src/Interfaces/interfaces.ts
@@ -1,3 +1,14 @@
 export interface IRegister {}
 export interface IPosts {}
 export interface IAuth {}
+
+export interface IUser {
+  id: string;
+  username: string;
+}
+
+export interface IRegisterState extends IUser {
+  isAuth: boolean;
+  user: IUser | null;
+  error: string | null;
+}
